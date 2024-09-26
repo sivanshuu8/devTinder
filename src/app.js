32e7f4c3b3a3e919c79859393b5d8d07a2441c60
Sivanshu <sivanshu@zowork.com>
2024-09-26 14:21:25 +0530

    exploring get, post routes

--- src/app.js
+++ src/app.js
@@ -3,8 +3,16 @@ const express = require('express');
 
 const app = express();
 
-app.use('/test',(req, res) => {
-    res.send('the test page');
+// app.use('/test',(req, res) => {
+//     res.send('the test page');
+// })
+
+app.get('/test',(req, res) => {
+    res.send({firstName:'Sivanshu', lastName:'Singh', profession:'engineer'});
+});
+
+app.post('/test',(req, res) => {
+    res.send('data saved successfully');
 })
 
 app.use((req, res) => {
